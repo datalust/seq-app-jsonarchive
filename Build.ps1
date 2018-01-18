00cfcc36025723d71d0b5211b4f362bbcaf2086c
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-01-19 08:58:22 +1000

    Don't include redundant build number in revision

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,10 @@
 # This script originally (c) 2016 Serilog Contributors - license Apache 2.0
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = @{ $true = ""; $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 $version = "1.0."
-if ($env:APPVEYOR_BUILD_NUMBER) {
+if ($env:APPVEYOR_BUILD_NUMBER -ne $NULL) {
     $version = $version + $env:APPVEYOR_BUILD_NUMBER
 } else {
     $version = $version + "0"
