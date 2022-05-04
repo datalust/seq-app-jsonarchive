70f7df8f3680f0c28081eb4990130bfb899efd0f
Liam McLennan <liam.mclennan@gmail.com>
2022-05-04 15:57:03 +1000

    added readableFilenames setting

--- src/main.rs
+++ src/main.rs
@@ -57,13 +57,20 @@ impl fmt::Display for AppError {
     }
 }
 
+#[derive(Copy,Clone)]
+enum FilenameFormat {
+    Timestamp,
+    Readable
+}
+
 struct FileSet<'a> {
     dir: &'a Path,
-    file_name_template: String
+    file_name_template: String,
+    format: FilenameFormat
 }
 
 impl<'a> FileSet<'a> {
-    fn new(file_set: &'a str) -> Result<FileSet<'a>, Box<dyn Error>> {
+    fn new(file_set: &'a str, format: FilenameFormat) -> Result<FileSet<'a>, Box<dyn Error>> {
         let file_set_path = Path::new(file_set);
 
         let dir = file_set_path.parent()
@@ -79,11 +86,16 @@ impl<'a> FileSet<'a> {
             Err(AppError("the filename pattern must include the `*` wildcard"))?
         };
 
-        Ok(FileSet{dir, file_name_template})
+        Ok(FileSet{dir, file_name_template, format })
     }
 
     fn make_file_path(&self, timestamp: DateTime<Utc>) -> PathBuf {
-        let timestamp = format!("{:01$x}", timestamp.timestamp(), 16);
+        const READABLE_FORMAT: &str = "%Y-%m-%dT%H_%M_%SZ";
+
+        let timestamp = match &self.format {
+            FilenameFormat::Timestamp => format!("{:01$x}", timestamp.timestamp(), 16),
+            FilenameFormat::Readable => format!("{}", timestamp.format(READABLE_FORMAT))
+        };
         let full_file_name = self.file_name_template.replace("*", &timestamp);
         let mut buf = self.dir.to_path_buf();
         buf.push(full_file_name);
@@ -110,11 +122,29 @@ impl<'a> FileSet<'a> {
     }
 }
 
+fn is_truthy(name: impl AsRef<str>) -> Result<bool, Box<dyn Error>> {
+    match env::var(name.as_ref()) {
+        // The evironment variable contains a truthy value
+        Ok(ref v) if v == "True" || v == "true" => return Ok(true),
+        // The environment variable is not set or doesn't contain
+        // a truthy value
+        Ok(_) | Err(env::VarError::NotPresent) => return Ok(false),
+        // The environment variable is invalid
+        Err(e) => Err(e)?,
+    }
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
     let file_set_var = env::var("SEQ_APP_SETTING_FILESET")
         .map_err(|_| AppError("the `SEQ_APP_SETTING_FILESET` environment variable is not set"))?;
 
-    let file_set = FileSet::new(&file_set_var)?;
+    let file_set = FileSet::new(
+        &file_set_var,
+        if is_truthy("SEQ_APP_READABLEFILENAMES")? {
+            FilenameFormat::Readable
+        } else {
+            FilenameFormat::Timestamp
+        })?;
 
     let chunk_size_var = env::var("SEQ_APP_SETTING_CHUNKSIZE")
         .unwrap_or(String::new());
@@ -126,7 +156,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     } else {
         104857600
     };
-    
+
     let stdin = io::stdin();
 
     let (mut file, mut current_len) = file_set.open_next_file()?;
@@ -187,7 +217,7 @@ mod tests {
     #[test]
     fn file_set_is_split() {
         let p = join_path(&["path", "to", "log-*.clef"]);
-        let file_set = FileSet::new(&p).unwrap();
+        let file_set = FileSet::new(&p, FilenameFormat::Timestamp).unwrap();
 
         let q = join_path(&["path", "to"]);
         assert_eq!(&q, file_set.dir_name());
@@ -197,11 +227,22 @@ mod tests {
     #[test]
     fn timestamped_file_path_constructed() {
         let p = join_path(&["path", "to", "log-*.clef"]);
-        let file_set = FileSet::new(&p).unwrap();
+        let file_set = FileSet::new(&p, FilenameFormat::Timestamp).unwrap();
         let timestamp = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2016, 7, 8).and_hms(9, 10, 11), Utc);
         let path = file_set.make_file_path(timestamp);
 
         let q = join_path(&["path", "to", "log-00000000577f6df3.clef"]);
+        assert_eq!(&q, path.to_str().unwrap());
+    }
+
+    #[test]
+    fn readable_file_path_constructed() {
+        let p = join_path(&["path", "to", "log-*.clef"]);
+        let file_set = FileSet::new(&p, FilenameFormat::Readable).unwrap();
+        let timestamp = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2016, 7, 8).and_hms(9, 10, 11), Utc);
+        let path = file_set.make_file_path(timestamp);
+
+        let q = join_path(&["path", "to", "log-2016-07-08T09_10_11Z.clef"]);
         assert_eq!(&q, path.to_str().unwrap());
     }
 }
